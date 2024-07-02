f99971cc82bdd31903bde2e57f7acb28b7c783be
Iago Engel Serafim <iagoengel@yahoo.com>
2024-07-02 20:01:36 -0300

    Aula 2(Estrutura básica do Router DOM)

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
+// import Cardapio from './pages/Cardapio';
+// import Inicio from 'pages/Inicio';
+// const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    {/* {componenteAtual} */}
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
